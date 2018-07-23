3b2b3d25b48f314020a25b72c5fe5c82f73c5a47
Carrie Savinell <carrielane89@gmail.com>
2018-07-23 16:02:19 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,12 +2,16 @@ class LandmarksController < ApplicationController
 
   get '/landmarks' do
     @landmarks = Landmark.all
-    @figures = Figure.all
     erb :'/landmarks/index'
   end
 
   get '/landmarks/new' do
     erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
   end
 
   post '/landmarks/show' do
